3ed78f5d90c1d6f4cf748b07677887b0d13260bc
guonchy@gmail.com <guonchy@gmail.com>
2025-05-19 13:28:32 +0000

    mid competitions

---
+++ models/staging/kaggle/stg_kaggle__competitions.sql
@@ -0,0 +1,29 @@
+with source_competitions as (
+    select * from {{ source('kaggle','competitions') }}
+),
+casted as (
+    competition_id::integer as competition_id,
+    competition_code::string as competition_code,
+    competition_code::string as name,
+    sub_type::string as sub_type,
+    case
+        when sub_type = 'first_tier' then 'domestic_league'
+        when sub_type = 'domestic_cup' then 'domestic_cup'
+        when sub_type in (
+            'uefa_champions_league', 'europa_league', 'uefa_europa_conference_league_qualifiers',
+            'uefa_champions_league_qualifying', 'europa_league_qualifying', 'uefa_europa_conference_league',
+            'uefa_super_cup'
+        ) then 'international_cup'
+        else 'other'
+    end as "type",
+    
+)
+
+select 
+    *,
+    case when competition_id in (
+        'ES1', 'GB1', 'IT1', 'FR1', 'L1'
+    ) then true
+    else false
+    end as is_major_national_league
+from source_competitions
